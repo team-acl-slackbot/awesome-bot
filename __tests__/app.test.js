6dfad73ace846841868ae07cee840eb6940a9dcb
Jake Thrasher <mjakethrasher@gmail.com>
2021-04-28 14:14:03 -0700

    test convertDueDate function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,13 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { convertDueDate } = require('../lib/utils/canvas-utils');
 
 describe('awesome-bot routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  it('should convert the date to correct format',()=>{
+    expect(convertDueDate("2021-04-29T16:00:00Z")).toEqual("4/29/2021, 9:00:00 AM")
+  })
 });
