2621ccaf344d036c53329651b86d04ea76d91d3d
zhaned <zhanedmond@gmail.com>
2021-04-29 11:32:29 -0700

    added additional test for zoom

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,9 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const { convertDueDate } = require('../lib/utils/canvas-utils');
-const { participantsChecker } = require('../lib/utils/zoom-utils');
+const { participantsChecker, aggregateBlock } = require('../lib/utils/zoom-utils');
+const { blockDataForTest } = require('../lib/zoom/blockDataForTest');
+
 
 describe('awesome-bot routes', () => {
   beforeEach(() => {
@@ -23,5 +25,22 @@ describe('awesome-bot routes', () => {
   it('returns none because no participants', ()=>{
     const dummyData = []
     expect(participantsChecker(dummyData)).toEqual(`None`)
+  })
+  it('returns a block', ()=>{
+    const blockData = [
+      {
+        _id: '5f58f91f7ad1c0ea0b995937',
+        zoomId: '498946105',
+        joinUrl: 'https://zoom.us/j/498946105?pwd=TnFCSmpRTDBua1ZlZGRObVc1U0VXdz09',
+        name: 'Community & Current Events',
+      },
+      {
+        _id: '5fb743a030e95f0de1d63b79',
+        zoomId: '2090393356',
+        joinUrl: 'https://zoom.us/j/2090393356?pwd=YmdBTUFCVlUwOHYvWkZSeXlZSHNLQT09',
+        name: "Mercury Room's Personal Meeting Room",
+      }
+    ]
+    expect(aggregateBlock(blockData)).toEqual(blockDataForTest)
   })
 });
