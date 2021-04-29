ed380d74322ff03e9e6f7e836c5374270472d435
zhaned <zhanedmond@gmail.com>
2021-04-29 10:17:47 -0700

    added tests for zoom function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const { convertDueDate } = require('../lib/utils/canvas-utils');
+const { participantsChecker } = require('../lib/utils/zoom-utils');
 
 describe('awesome-bot routes', () => {
   beforeEach(() => {
@@ -10,5 +11,17 @@ describe('awesome-bot routes', () => {
   });
   it('should convert the date to correct format',()=>{
     expect(convertDueDate("2021-04-29T16:00:00Z")).toEqual("4/29/2021, 4:00:00 PM")
+  })
+  it('return the list of participants', ()=>{
+    const dummyData = [
+      'Jane',
+      'Doe',
+      'John'
+    ]
+    expect(participantsChecker(dummyData)).toEqual(`\n - Jane, \n - Doe, \n - John`)
+  })
+  it('returns none because no participants', ()=>{
+    const dummyData = []
+    expect(participantsChecker(dummyData)).toEqual(`None`)
   })
 });
